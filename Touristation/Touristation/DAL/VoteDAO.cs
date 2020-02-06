87fc17d992a92853d6fbe6cde5481a771085ea1e
Megantee1805 <44191183+Megantee1805@users.noreply.github.com>
2020-02-06 23:04:32 +0800

    finished votes insert and delete

---
+++ Touristation/Touristation/DAL/VoteDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Touristation.BLL;
+
+namespace Touristation.DAL
+{
+    public class VoteDAO
+    {
+        public void Insert(Vote count)
+        {
+            using (TouristationEntityModel db = new TouristationEntityModel())
+            {
+                Vote vote = new Vote();
+                db.Votes.Add(count);
+                db.SaveChanges(); 
+            }
+        }
+
+
+        public void Delete(int id)
+        {
+            using (TouristationEntityModel db = new TouristationEntityModel())
+            {
+                Vote vote = new Vote();
+                db.Votes.Remove(db.Votes.Single(u => u.Id == id));
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Touristation/Touristation/Model_Methods/VoteDAM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Touristation.DAL;
+
+namespace Touristation.BLL
+{
+    public partial class Vote
+    {
+        public void CastVotes(Vote count)
+        {
+            VoteDAO voting = new VoteDAO();
+            voting.Insert(count); 
+        }
+
+        public void RemoveVotes(int id)
+        {
+            VoteDAO voting = new VoteDAO();
+            voting.Delete(id); 
+        }
+    }
+}
